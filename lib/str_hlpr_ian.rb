72dc2cd5da40530b042261c63dd17c4860503c59
Ian Harwell <sirharwell@gmail.com>
2018-04-18 10:10:52 -0600

    gem

--- lib/str_hlpr_ian.rb
+++ lib/str_hlpr_ian.rb
@@ -1,5 +1,25 @@
 require "str_hlpr_ian/version"
 
 module StrHlprIan
-  # Your code goes here...
+  def self.reversify(str)
+    str.split('').reverse.join('')
+  end
+
+  def self.casify(str)
+    to_case = [:upcase, :downcase]
+    arr = str.split('')
+    arr.each_with_index do |letter, i|
+      this_case = to_case[rand(2)]
+      arr[i] = letter.send(this_case)
+    end
+    arr.join('')
+
+    def self.spacify(str, spaces = 0)
+      new_string = str
+      spaces.times do
+        new_string = new_string
+          .split('')
+          .join(' ')
+        end
+        new_string
 end
